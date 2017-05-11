46402e8bdecedefb878dc3b598cc85cafbb07844
NightCatSama <642163903@qq.com>
2017-05-11 17:49:12 +0800

    modify tabBar demo

--- src/pages/demo/TabBar.vue
+++ src/pages/demo/TabBar.vue
@@ -1,23 +1,23 @@
 <template>
   <demo title="TabBar" class="tabBar-demo">
     <demo-item name="default" description="默认" :code="code" slot="left">
-      <TabBar :data="arr" v-model="default_index" @change="(i) => default_index = i"></TabBar>
+      <TabBar :data="arr" v-model="default_index"></TabBar>
     </demo-item>
 
     <demo-item name="type" description="类型" :code="type_code" slot="right">
-      <TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="no_padding"></TabBar>
-      <TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="fix_width"></TabBar>
-      <TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="fix_width:25%"></TabBar>
+      <TabBar :data="arr" v-model="type_index" type="no_padding"></TabBar>
+      <TabBar :data="arr" v-model="type_index" type="fix_width"></TabBar>
+      <TabBar :data="arr" v-model="type_index" type="fix_width:25%"></TabBar>
     </demo-item>
 
     <demo-item name="theme" description="主题" :code="theme_code" slot="left">
-      <TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i"></TabBar>
-      <TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i" theme="card"></TabBar>
-      <TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i" theme="ticket" type="fix_width"></TabBar>
+      <TabBar :data="arr" v-model="theme_index"></TabBar>
+      <TabBar :data="arr" v-model="theme_index" theme="card"></TabBar>
+      <TabBar :data="arr" v-model="theme_index" theme="ticket" type="fix_width"></TabBar>
     </demo-item>
 
     <demo-item name="trigger" description="触发类型" :code="trigger_code" slot="right">
-      <TabBar :data="arr" v-model="trigger_index" @change="(i) => trigger_index = i" trigger="hover"></TabBar>
+      <TabBar :data="arr" v-model="trigger_index" trigger="hover"></TabBar>
     </demo-item>
   </demo>
 </template>
@@ -36,20 +36,20 @@
         theme_index: 0,
         trigger_index: 0,
         code: `
-<TabBar :data="arr" v-model="default_index" @change="(i) => default_index = i"></TabBar>
+<TabBar :data="arr" v-model="default_index"></TabBar>
         `,
         type_code: `
-<TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="no_padding"></TabBar>
-<TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="fix_width"></TabBar>
-<TabBar :data="arr" v-model="type_index" @change="(i) => type_index = i" type="fix_width:25%"></TabBar>
+<TabBar :data="arr" v-model="type_index" type="no_padding"></TabBar>
+<TabBar :data="arr" v-model="type_index" type="fix_width"></TabBar>
+<TabBar :data="arr" v-model="type_index" type="fix_width:25%"></TabBar>
         `,
         theme_code: `
-<TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i"></TabBar>
-<TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i" theme="card"></TabBar>
-<TabBar :data="arr" v-model="theme_index" @change="(i) => theme_index = i" theme="ticket" type="fix_width"></TabBar>
+<TabBar :data="arr" v-model="theme_index"></TabBar>
+<TabBar :data="arr" v-model="theme_index" theme="card"></TabBar>
+<TabBar :data="arr" v-model="theme_index" theme="ticket" type="fix_width"></TabBar>
         `,
         trigger_code: `
-<TabBar :data="arr" v-model="trigger_index" @change="(i) => trigger_index = i" trigger="hover"></TabBar>
+<TabBar :data="arr" v-model="trigger_index" trigger="hover"></TabBar>
         `
       }
     }

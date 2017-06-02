7b216da52b617a7b70c3705635185ea5a2b5b54c
cyh <cyh@hitour.cc>
2017-06-02 16:36:32 +0800

    save: Swipe arrow style

--- src/components/Swipe/components/arrow.vue
+++ src/components/Swipe/components/arrow.vue
@@ -2,12 +2,13 @@
   <div class="swipe-arrow">
     <div class="swipe-button"
          v-for="(direction, index) in ['left', 'right']"
-         :class="[direction, { disabled: arrow_disabled_status[direction], outer: outer_arrow}]"
+         v-show="!arrow_disabled_status[direction]"
+         :class="[direction, { outer: outer_arrow}]"
          :style="{ 'margin-top': -arrow_offset + 'px' }">
 
       <!-- outer arrow -->
-      <span class="arrow outer" :class="direction" v-if="outer_arrow" @click="direction === 'left' ? swiper.slidePrev() : swiper.slideNext()">
-        <Icon class="icon" :name="direction" :size="24" color="#767676"></Icon>
+      <span class="arrow outer" :class="direction" v-if="outer_arrow" @click="direction === 'left' ? swiper.slidePrev() : swiper.slideNext()" @mouseenter="$set(hover, index, true)" @mouseleave="$set(hover, index, false)">
+        <Icon class="icon" :name="direction" :size="24" :color="hover[index] ? '#323232' : '#999'"></Icon>
       </span>
 
       <!-- inner arrow -->
@@ -15,7 +16,6 @@
         <Icon class="icon" :name="`arrow${direction}`" :size="16" color="#fff"></Icon>
       </span>
 
-      <div class="disabled-mask"></div>
     </div>
   </div>
 </template>
@@ -25,10 +25,6 @@
   .swipe-arrow { position:absolute;z-index:9;top:50%;transform: translateY(-50%);width:100%;height:0 }
   .swipe-button {
     width: _size_md; height: _size_md; position:absolute;z-index:9;top:50%;transform: translateY(-50%);margin:0;cursor:pointer;
-    &.disabled {
-      .disabled-mask { display:block }
-      .arrow { opacity:.5 }
-    }
     &.left {
       left: _gap_xsm - 2;
       &.outer { left: - + _gap_sm - 5 }
@@ -50,11 +46,11 @@
     .arrow{
       transition: transform .3s, background-color .3s;position: absolute;width: _size_md;height: _size_md;display: block;border-radius: 100%;background-color: rgba(0, 0, 0, .2);
       &.outer { border-radius: 0;background-color:transparent;height:_size_o_md;width: _size_o_md*0.5 }
+      &:not(.outer):hover{ background-color: rgba(0, 0, 0, .4) }
       .icon{
         position: absolute;left:50%;top:50%;transform: translate(-50%,-50%);
       }
     }
-    .disabled-mask { position:absolute;z-index:9;left:0;top:0;width:100%;height:100%;display:none;background-color:transparent;cursor:not-allowed }
   }
 </style>
 
@@ -63,7 +59,9 @@
     name: 'swipeArrow',
     props: ['outer_arrow', 'arrow_offset', 'loop', 'preview', 'swiper', 'slides_len'],
     data: function () {
-      return {}
+      return {
+        hover: [false, false]
+      }
     },
     computed: {
       arrow_disabled_status: function () {
